80afd9db0de5bc9cb3579826ad3e9b4d30efa46a
rodolfo.dutra <rodolfo.dutra@d25.io>
2019-12-05 13:59:29 +0100

    add markdown loader, add route for blog post

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p v-html="content"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params, app }) {
+    const fileContent = await import(`~/contents/blog/${params.slug}.md`)
+    const attr = fileContent.attributes
+    return {
+      name: params.slug,
+      title: attr.title,
+      year: attr.year,
+      content: fileContent.html
+    }
+  }
+}
+</script>
