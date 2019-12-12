8ddccd25e507465bb7898bb60834bbf034ac4e3a
rodolfo.dutra <rodolfo.dutra@d25.io>
2019-12-12 14:05:55 +0100

    add html lang

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,9 @@ export default {
    ** Headers of the page
    */
   head: {
+    htmlAttrs: {
+      lang: 'en-EN'
+    },
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
